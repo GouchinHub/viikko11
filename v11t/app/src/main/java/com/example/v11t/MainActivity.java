18a6e5d73c4b4488d5b91e765c54d584febb948a
GouchinHub <69035493+GouchinHub@users.noreply.github.com>
2021-04-06 15:14:57 +0300

    Versio1.0

--- v11t/app/src/main/java/com/example/v11t/MainActivity.java
+++ v11t/app/src/main/java/com/example/v11t/MainActivity.java
@@ -3,78 +3,134 @@ package com.example.v11t;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
-import androidx.core.view.MenuItemCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-    private DrawerLayout drawer;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawer;
+    FragmentManager manager;
     TextViewFragment fragment;
+    Spinner fontOptions, widthOptions, heightOptions, rowOptions, languageOptions;
+    EditText title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawer = findViewById(R.id.drawer_layout);
-        fragment = (TextViewFragment) getSupportFragmentManager().findFragmentById(R.id.textView_fragment);
 
+        drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        Menu menu = navigationView.getMenu();
+        //create new fragment and commit to manager
+        fragment = new TextViewFragment();
+        manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.fragment_container,
+                fragment).commit();
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                new TextViewFragment()).commit();
+        populateSpinners(menu);
+        title = (EditText) menu.findItem(R.id.give_title).getActionView().findViewById(R.id.change_title);
+
+        //change default toolbar new toolbar with menu button to open the navigation drawer
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
-                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        Menu menu = navigationView.getMenu();
+        //create listener for switch in menu
         Switch actionView = (Switch) menu.findItem(R.id.edit_switch).getActionView().findViewById(R.id.editSwitch);
+        actionView.setChecked(true);
         actionView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
+                if (isChecked) {
                     fragment.changeEnabled();
-                }
-                else {
+                } else {
                     fragment.changeDisabled();
                 }
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater infater = getMenuInflater();
-        infater.inflate(R.menu.drawer_menu,menu);
-        return true;
+    //Populate all spinners in drawer menu with string arrays
+    public void populateSpinners(Menu menu) {
+        fontOptions = (Spinner) menu.findItem(R.id.font_size).getActionView().findViewById(R.id.spinner);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.fontSizes));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fontOptions.setAdapter(myAdapter);
+
+        widthOptions = (Spinner) menu.findItem(R.id.width).getActionView().findViewById(R.id.spinner);
+        ArrayAdapter<String> myAdapter2 = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.widths));
+        myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        widthOptions.setAdapter(myAdapter2);
+
+        heightOptions = (Spinner) menu.findItem(R.id.height).getActionView().findViewById(R.id.spinner);
+        ArrayAdapter<String> myAdapter3 = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.heights));
+        myAdapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        heightOptions.setAdapter(myAdapter3);
+
+        rowOptions = (Spinner) menu.findItem(R.id.row_count).getActionView().findViewById(R.id.spinner);
+        ArrayAdapter<String> myAdapter4 = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.rows));
+        myAdapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        rowOptions.setAdapter(myAdapter4);
+
+        languageOptions = (Spinner) menu.findItem(R.id.change_language).getActionView().findViewById(R.id.spinner);
+        ArrayAdapter<String> myAdapter5 = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.languages));
+        myAdapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageOptions.setAdapter(myAdapter5);
+    }
+
+    //change application language and restart activity to see changes
+    //NOTE!: this could be done without restart by using the new language resource and manually changing all UI elements
+    public void changeLanguage(String language) {
+        LocaleHelper.setLocale(MainActivity.this, language);
+        finish();
+        startActivity(getIntent());
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.edit_switch:
-                //TODO
+        switch (item.getItemId()) {
+            case R.id.change_language:
+                if (languageOptions.getSelectedItemId() == 1) {
+                    changeLanguage("en");
+                } else if (languageOptions.getSelectedItemId() == 2) {
+                    changeLanguage("fi");
+                }
+                break;
+            case R.id.give_title:
+                fragment.changeTitle(title.getText().toString());
+                break;
+            case R.id.font_size:
+                fragment.changeFont(Integer.parseInt(fontOptions.getSelectedItem().toString()));
+                break;
+            case R.id.width:
+                fragment.changeWidth(Integer.parseInt(widthOptions.getSelectedItem().toString()));
+                break;
+            case R.id.height:
+                fragment.changeHeight(Integer.parseInt(heightOptions.getSelectedItem().toString()));
+                break;
+            case R.id.row_count:
+                fragment.changeRowCount(Integer.parseInt(rowOptions.getSelectedItem().toString()));
                 break;
         }
         return true;
@@ -82,17 +138,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)){
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
-    }
-    public void onClick(View v){
-        //fragment = new Fragment();
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        //transaction.replace(R.id.nav,fragment);
-        transaction.commit();
     }
 }
--- v11t/app/src/main/java/com/example/v11t/TextViewFragment.java
+++ v11t/app/src/main/java/com/example/v11t/TextViewFragment.java
@@ -1,6 +1,8 @@
 package com.example.v11t;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +16,9 @@ import androidx.fragment.app.Fragment;
 public class TextViewFragment extends Fragment {
 
     private EditText writeField;
-    private TextView readField;
+    private TextView readField, rfTitle;
+    private ViewGroup.LayoutParams params;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,14 +29,46 @@ public class TextViewFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         writeField = view.findViewById(R.id.writeView);
         readField = view.findViewById(R.id.readView);
+        rfTitle = view.findViewById(R.id.readViewTitle);
         writeField.setEnabled(true);
+        params = readField.getLayoutParams();
         super.onViewCreated(view, savedInstanceState);
     }
 
-    public void changeEnabled(){
+    public void changeTitle(String title) {
+        rfTitle.setText(title);
+    }
+
+    public void changeFont(int fontsize) {
+        readField.setTextSize(TypedValue.COMPLEX_UNIT_DIP, fontsize);
+    }
+
+    public void changeWidth(int width) {
+        params.width = width;
+        readField.setLayoutParams(params);
+    }
+
+    public void changeHeight(int height) {
+        params.height = height;
+        readField.setLayoutParams(params);
+    }
+
+    public void changeRowCount(int count) {
+        readField.setLines(count);
+    }
+
+    public void changeEnabled() {
+        readField.setText("");
         writeField.setEnabled(true);
+        writeField.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
     }
-    public void changeDisabled(){
+
+    //disable EditText field and pass text to TextView
+    public void changeDisabled() {
+        readField.setText(writeField.getText().toString());
         writeField.setEnabled(false);
+        writeField.setInputType(InputType.TYPE_NULL);
     }
+
+
 }
